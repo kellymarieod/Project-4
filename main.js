1eed29ecd98d697204ff2b47b06b272070ba98b6
kellymarieod <kellymarieod@gmail.com>
2016-05-17 22:34:27 -0400

    java attempt at scroll stuff not working

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-$(document).ready( alert("ready"))
+$(document).ready( function() {
+  var lastScrollTop = 0;
+  $(@pageContainer).scroll(function () {
+    var currentScrollPosition = $(this).scrollTop();
+    if (currentScrollPosition > lastScrollTop) {
+      $(@lap1).background-color("red");
+    }
+    else {
+      $(@lap1).background-color("blue");
+    }
+    lastScrollTop = currentScrollPosition;
+  });
+
+});
+
+
+
+
+
+
+  // alert("ready"))
