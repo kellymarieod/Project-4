4bbb54b3ace762dcb96833ad7c861756a83301f5
kellymarieod <kellymarieod@gmail.com>
2016-05-17 22:51:30 -0400

    more failed java attempts at scrolling image

--- main.js
+++ main.js
@@ -1,17 +1,54 @@
 $(document).ready( function() {
-  var lastScrollTop = 0;
-  $(@pageContainer).scroll(function () {
-    var currentScrollPosition = $(this).scrollTop();
-    if (currentScrollPosition > lastScrollTop) {
-      $(@lap1).background-color("red");
-    }
-    else {
-      $(@lap1).background-color("blue");
-    }
-    lastScrollTop = currentScrollPosition;
-  });
 
-});
+
+
+
+
+  // $("div").click(function () {
+  //       $(this).hide("slide", { direction: "left" }, 1000);
+  // });
+
+
+
+
+  //
+  // var pageContainer = $(".page-container");
+  // var picDivider = $(".picDivider");
+  // var offset = 0;
+  //
+  // $(window).scroll( function() {
+  //   offset = $(window).scrollTop() - pageContainer.offset().top;
+  //   // console.log(".page-container offset top " + pageContainer.offset().top);
+  //       // console.log("window scroll top " + $(window).scrollTop());
+  //       // console.log("offset " + offset);
+  //       if (offset > 100 && !$(footer).hasClass("shrink")) {
+  //         picDivider.addClass("shrink");
+  //         // console.log("added class of 'shrink' to .footer");
+  //       } else if (offset <= 100 && picDivider.hasClass("shrink")) {
+  //         picDivider.removeClass("shrink");
+  //         // console.log("removed class of 'shrink' from .footer");
+  //       } else {
+  //         return;
+  //       }
+  //     });
+  //   });
+  //
+
+//
+// $(document).ready( function() {
+//   var lastScrollTop = 0;
+//   $(@pageContainer).scroll(function () {
+//     var currentScrollPosition = $(this).scrollTop();
+//     if (currentScrollPosition > lastScrollTop) {
+//       $(@lap1).background-color("red");
+//     }
+//     else {
+//       $(@lap1).background-color("blue");
+//     }
+//     lastScrollTop = currentScrollPosition;
+//   });
+//
+// });
 
 
 
