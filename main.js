f8bdcb723bd61060d0e7b9586e3fd886cd7ba34d
kellymarieod <kellymarieod@gmail.com>
2016-05-18 20:13:43 -0400

    commented out last failed attempt to get java scroll function to work

--- main.js
+++ main.js
@@ -1,58 +1,78 @@
-$(document).ready( function() {
-
-
-
-
-
-  // $("div").click(function () {
-  //       $(this).hide("slide", { direction: "left" }, 1000);
-  // });
-
-
-
-
-  //
-  // var pageContainer = $(".page-container");
-  // var picDivider = $(".picDivider");
-  // var offset = 0;
-  //
-  // $(window).scroll( function() {
-  //   offset = $(window).scrollTop() - pageContainer.offset().top;
-  //   // console.log(".page-container offset top " + pageContainer.offset().top);
-  //       // console.log("window scroll top " + $(window).scrollTop());
-  //       // console.log("offset " + offset);
-  //       if (offset > 100 && !$(footer).hasClass("shrink")) {
-  //         picDivider.addClass("shrink");
-  //         // console.log("added class of 'shrink' to .footer");
-  //       } else if (offset <= 100 && picDivider.hasClass("shrink")) {
-  //         picDivider.removeClass("shrink");
-  //         // console.log("removed class of 'shrink' from .footer");
-  //       } else {
-  //         return;
-  //       }
-  //     });
-  //   });
-  //
-
-//
 // $(document).ready( function() {
-//   var lastScrollTop = 0;
-//   $(@pageContainer).scroll(function () {
-//     var currentScrollPosition = $(this).scrollTop();
-//     if (currentScrollPosition > lastScrollTop) {
-//       $(@lap1).background-color("red");
-//     }
-//     else {
-//       $(@lap1).background-color("blue");
-//     }
-//     lastScrollTop = currentScrollPosition;
-//   });
 //
-// });
-
-
-
-
-
-
-  // alert("ready"))
+//   var pageContainer = $(".page-container");
+//   var blueBananas = $(".blueBananas");
+//   var offset = 0;
+//
+//   $(window).scroll( function() {
+//     offset = $(window).scrollTop() - pageContainer.offset().top;
+//     // console.log(".page-container offset top " + pageContainer.offset().top);
+//         // console.log("window scroll top " + $(window).scrollTop());
+//         // console.log("offset " + offset);
+//         if (offset > 100 && !$(blueBananas).hasClass("blue")) {
+//           blueBananas.addClass("blue");
+//           // console.log("added class of 'shrink' to .footer");
+//         } else if (offset <= 100 && blueBananas.hasClass("blue")) {
+//           blueBananas.removeClass("blue");
+//           // console.log("removed class of 'shrink' from .footer");
+//         } else {
+//           return;
+//         }
+//       });
+//     });
+//
+// 
+// }
+//
+//   // $("div").click(function () {
+//   //       $(this).hide("slide", { direction: "left" }, 1000);
+//   // });
+//
+//
+//
+//
+//   //
+//   // var pageContainer = $(".page-container");
+//   // var picDivider = $(".picDivider");
+//   // var offset = 0;
+//   //
+//   // $(window).scroll( function() {
+//   //   offset = $(window).scrollTop() - pageContainer.offset().top;
+//   //   // console.log(".page-container offset top " + pageContainer.offset().top);
+//   //       // console.log("window scroll top " + $(window).scrollTop());
+//   //       // console.log("offset " + offset);
+//   //       if (offset > 100 && !$(footer).hasClass("shrink")) {
+//   //         picDivider.addClass("shrink");
+//   //         // console.log("added class of 'shrink' to .footer");
+//   //       } else if (offset <= 100 && picDivider.hasClass("shrink")) {
+//   //         picDivider.removeClass("shrink");
+//   //         // console.log("removed class of 'shrink' from .footer");
+//   //       } else {
+//   //         return;
+//   //       }
+//   //     });
+//   //   });
+//   //
+//
+// //
+// // $(document).ready( function() {
+// //   var lastScrollTop = 0;
+// //   $(@pageContainer).scroll(function () {
+// //     var currentScrollPosition = $(this).scrollTop();
+// //     if (currentScrollPosition > lastScrollTop) {
+// //       $(@lap1).background-color("red");
+// //     }
+// //     else {
+// //       $(@lap1).background-color("blue");
+// //     }
+// //     lastScrollTop = currentScrollPosition;
+// //   });
+// //
+// // });
+//
+//
+//
+//
+//
+//
+//   // alert("ready"))
